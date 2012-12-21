c40d08e6dbb664cb30345a91dbb33ef8ac1a9c8e
Jaynti Kanani <jdkanani@gmail.com>
2012-12-21 14:53:02 +0530

    remove `backbone-relational` reference from speed-suite

--- test/speed.js
+++ test/speed.js
@@ -1,4 +1,4 @@
-(function(){  	    
+(function () {
     var employeeCollection10 = Utility.getEmployee(10);
     var employeeCollection15 = Utility.getEmployee(15);
     var employeeCollection20 = Utility.getEmployee(20);
@@ -7,17 +7,17 @@
 
     // Associated Model
     var associatedModel = {};
-    associatedModel.Location = Backbone.AssociatedModel.extend({		  
+    associatedModel.Location = Backbone.AssociatedModel.extend ({
         defaults: {
             add1 : "",
             add2 : null,
             zip : "",
             state : ""
-        }        
+        }
     });
-    
-    associatedModel.Department = Backbone.AssociatedModel.extend({				 			  
-        relations: [            
+
+    associatedModel.Department = Backbone.AssociatedModel.extend ({
+        relations: [
             {
                 type: Backbone.Many,
                 key: 'locations',
@@ -27,11 +27,11 @@
         defaults:{
             name : '',
             locations : [],
-            number :-1            
+            number :-1
         }
     });
 
-    associatedModel.Employee = Backbone.AssociatedModel.extend({		  
+    associatedModel.Employee = Backbone.AssociatedModel.extend ({
         relations: [
             {
                 type: Backbone.One,
@@ -47,12 +47,12 @@
             age : 0,
             fname : "",
             lname : "",
-            works_for:{}            
-        }        
-    });        
-    
-    associatedModel.Company =  Backbone.AssociatedModel.extend({				 			  
-        relations: [            
+            works_for:{}
+        }
+    });
+
+    associatedModel.Company =  Backbone.AssociatedModel.extend ({
+        relations: [
             {
                 type: Backbone.Many,
                 key: 'employees',
@@ -61,112 +61,29 @@
         ],
         defaults:{
             name : '',
-            employees : []                     
+            employees : []
         }
     });
 
-    // Relational Model    
-    var relationalModel = {};
-    relationalModel.Location = Backbone.RelationalModel.extend({		  
-        defaults: {
-            add1 : "",
-            add2 : null,
-            zip : "",
-            state : ""
-        }        
-    });
-    
-    relationalModel.Department = Backbone.RelationalModel.extend({				 			  
-        relations: [            
-            {
-                type: Backbone.HasMany,
-                key: 'locations',
-                relatedModel: relationalModel.Location
-            }
-        ],
-        defaults:{
-            name : '',
-            locations : [],
-            number :-1            
-        }
-    });
-
-    relationalModel.Employee = Backbone.RelationalModel.extend({		  
-        relations: [
-            {
-                type: Backbone.HasOne,
-                key: 'works_for',
-                relatedModel: relationalModel.Department
-            }
-        ],
-        validate:function(attr){
-           return (attr.sex && attr.sex != "M" && attr.sex != "F") ? "invalid sex value" : undefined;
-        },
-        defaults: {
-            sex : 'M', //{F,M}
-            age : 0,
-            fname : "",
-            lname : "",
-            works_for:{}            
-        }        
-    });	
-    
-    relationalModel.Company =  Backbone.RelationalModel.extend({				 			  
-        relations: [            
-            {
-                type: Backbone.HasMany,
-                key: 'employees',
-                relatedModel: relationalModel.Employee
-            }
-        ],
-        defaults:{
-            name : '',
-            employees : []                     
-        }
-    });
-
-
-    //JSLitmus Backbone-associations Test Cases    
-    JSLitmus.test('Backbone-associations:Set : 10 inserts', function() {
+    //JSLitmus Backbone-associations Test Cases
+    JSLitmus.test ('Set : 10 inserts', function() {
         var company = new associatedModel.Company();
-        company.set(employeeCollection10);		
-    });	
-    JSLitmus.test('Backbone-associations:Set : 15 inserts', function() {
-        var company = new associatedModel.Company();
-        company.set(employeeCollection15);		
-    });	
-    JSLitmus.test('Backbone-associations:Set : 20 inserts', function() {
-        var company = new associatedModel.Company();
-        company.set(employeeCollection20);		
-    });	
-    JSLitmus.test('Backbone-associations:Set : 25 inserts', function() {
-        var company = new associatedModel.Company();
-        company.set(employeeCollection25);		
+        company.set(employeeCollection10);
     });
-    JSLitmus.test('Backbone-associations:Set : 30 inserts', function() {
+    JSLitmus.test ('Set : 15 inserts', function() {
         var company = new associatedModel.Company();
-        company.set(employeeCollection30);		
-    });	
-
-    //JSLitmus Backbone-Relational Test Cases    
-    JSLitmus.test('Backbone-Relational:Set : 10 inserts', function() {
-        var company = new relationalModel.Company();
-        company.set(employeeCollection10);		
-    });	
-    JSLitmus.test('Backbone-Relational:Set : 15 inserts', function() {
-        var company = new relationalModel.Company();
-        company.set(employeeCollection15);		
-    });	
-    JSLitmus.test('Backbone-Relational:Set : 20 inserts', function() {
-        var company = new relationalModel.Company();
-        company.set(employeeCollection20);		
-    });	
-    JSLitmus.test('Backbone-Relational:Set : 25 inserts', function() {
-        var company = new relationalModel.Company();
-        company.set(employeeCollection25);		
+        company.set(employeeCollection15);
     });
-    JSLitmus.test('Backbone-Relational:Set : 30 inserts', function() {
-        var company = new relationalModel.Company();
-        company.set(employeeCollection30);		
-    });		
+    JSLitmus.test ('Set : 20 inserts', function() {
+        var company = new associatedModel.Company();
+        company.set(employeeCollection20);
+    });
+    JSLitmus.test ('Set : 25 inserts', function() {
+        var company = new associatedModel.Company();
+        company.set(employeeCollection25);
+    });
+    JSLitmus.test ('Set : 30 inserts', function() {
+        var company = new associatedModel.Company();
+        company.set(employeeCollection30);
+    });
 })();
