a06d6888f74110d3eaf0e207613865b315e74bd4
Jaynti Kanani <jdkanani@gmail.com>
2013-01-01 18:36:49 +0530

    added `window.console` if not available, for test suite

--- test/associated-model.js
+++ test/associated-model.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+
+    if ( !window.console ) {
+        window.console = {};
+        var names = [ 'log', 'debug', 'info', 'warn', 'error', 'assert', 'dir', 'dirxml',
+                    'group', 'groupEnd', 'time', 'timeEnd', 'count', 'trace', 'profile', 'profileEnd' ];
+        for ( var i = 0; i < names.length; ++i ) {
+            window.console[ names[i] ] = function() {};
+        }
+    }
+
     var attr
         , child1, child2, child3, child4
         , dept1
