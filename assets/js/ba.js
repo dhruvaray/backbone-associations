8bbe270623c16297fb44cd03f36844b5615aa0be
dhruvaray <dhruva_ray@persistent.co.in>
2013-06-20 17:39:44 +0530

    Remove unwanted code

--- assets/js/ba.js
+++
@@ -1,8 +0,0 @@
-$('body').delegate('.menu li a', 'click', function () {
-    var $thisLi = $(this).parents('li:first');
-    var $ul = $thisLi.parents('ul:first');
-    if (!$thisLi.hasClass('active')) {
-        $ul.find('li.active').removeClass('active');
-        $thisLi.addClass('active');
-    }
-});
