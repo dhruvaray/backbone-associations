8a9e583bee95a376030162b47691729b4eec5fec
Omi Chowdhury <omi@mangosix.co.uk>
2013-08-15 12:57:07 +1000

    This commit would fix #58

--- backbone-associations.js
+++ backbone-associations.js
@@ -374,8 +374,8 @@
 
         // Process all pending events after the entire object graph has been updated
         _processPendingEvents:function () {
-            if (!this.visited) {
-                this.visited = true;
+            if (!this.eventsVisited) {
+                this.eventsVisited = true;
 
                 this._deferEvents = false;
 
@@ -392,7 +392,7 @@
                     val && val._processPendingEvents();
                 }, this);
 
-                delete this.visited;
+                delete this.eventsVisited;
             }
         },
 
